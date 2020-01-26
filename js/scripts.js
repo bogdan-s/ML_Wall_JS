0613d0a867751001ead3b3ed0e13ea6259d0c317
bogdan-stanciulescu <53901758+bogdan-s@users.noreply.github.com>
2020-01-26 10:36:59 +0200

    update script JS

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+var canvas = document.getElementById('myCanvas'), 
+    context = canvas.getContext('2d');
+
+window.addEventListener('DOMContentLoad', initImageLoader);
+
+function initImageLoader() {
+    var location = window.location.href.replace(/\/+$/, "");
+    loadFile(location + './images/pic_the_scream.jpg');
+}
+
+function loadFile(file) {
+    var tempImageStore = new Image();
+
+    // set up the onload function - it won't run this code wntil the page loads
+    tempImageStore.onload = function(ev) {
+        // Gets the canvas width and height, sets the canvas size
+        canvas.height = ev.target.height;
+        canvas.width = ev.target.width;
+
+        // Draw the image onto the canvas
+        context.drawImage(ev.target, 0, 0);
+    }
+
+    tempImageStore.src = file;
+
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
